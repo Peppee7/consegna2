0cc9223feefdfd8c0a319a58fa76799b4ce59a9b
Peppee7 <giuseppesanese07@outlook.it>
2024-10-01 15:33:26 +0200

    ultimazione

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,29 @@
-function generateRandom(min, max) {
-    let n = Math.random()
+let tentativi = 0
+let n = 0
 
-    return Math.floor(n);
+function getRandom(max) {
+    n = Math.floor(Math.random() * max)
+
+    return n;
 }
 
-generateRandom(1, 100)
+getRandom(100)
+
+while(tentativi < 5) {
+    let colpo = prompt("Idovina il numero!!!")
+    while(colpo === null || isNaN(colpo)) {
+        colpo = prompt("Idovina il numero!!!")
+    }
+    tentativi++
+    colpo = parseInt(colpo)
+
+    if(colpo == n) {
+        console.log("CONGRATURAZIONI! Hai vinto.")
+    } else if(colpo < n) {
+        console.log("Troppo piccolo. Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+    } else {
+        console.log("Troppo grande.  Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+    }
+}
+
+alert("Hai perso. Il numero da indovinare era: " + n)
