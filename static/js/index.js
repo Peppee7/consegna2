2abda61cd416cd398aef7c47093a47da564889fd
Peppee7 <164175278+Peppee7@users.noreply.github.com>
2024-10-08 09:49:50 +0200

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,36 @@
 let tentativi = 0
 let n = 0
 
-function getRandom(max) {
-    n = Math.floor(Math.random() * max)
+function getRandom(min = 0, max = 100) {
+    n = Math.floor(Math.random() * (max - min + 1) - min)
 
     return n;
 }
 
-getRandom(100)
-alert(n)
-while(tentativi < 5) {
-    let colpo = prompt("Idovina il numero!!!")
-    while(colpo === null || isNaN(colpo)) {
-        colpo = prompt("Idovina il numero!!!")
-    }
-    tentativi++
-    colpo = parseInt(colpo)
+getRandom(0, 100)
 
-    if(colpo == n) {
-        console.log("CONGRATURAZIONI! Hai vinto.")
-        tentativi = 5
-    } else if(colpo < n) {
-        console.log("Troppo piccolo. Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
-    } else {
-        console.log("Troppo grande.  Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+function handleClick(event) { 
+
+    while(tentativi < 5) {
+        let colpo = document.querySelector("#guess")
+        console.log(userField.value)
+        while(colpo === null || isNaN(colpo)) {
+            colpo = prompt("Idovina il numero!!!")
+        }
+        tentativi++
+        colpo = parseInt(colpo)
+
+        if(colpo == n) {
+            console.log("CONGRATURAZIONI! Hai vinto.")
+            tentativi = 5
+        } else if(colpo < n) {
+            console.log("Troppo piccolo. Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+        } else {
+            console.log("Troppo grande.  Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+        }
     }
+
+    alert("Hai perso. Il numero da indovinare era: " + n)
 }
 
-alert("Hai perso. Il numero da indovinare era: " + n)
+btn.addEventListener("click", handleClick)
