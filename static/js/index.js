46d40eac509d00f15cfb74bda4e1fb2db445f982
Peppee7 <giuseppesanese07@outlook.it>
2024-10-14 17:27:22 +0200

    finish

--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,37 @@
-let tentativi = 0
+let tentativi = 5
 let n = 0
 
 function getRandom(min = 0, max = 100) {
-    n = Math.floor(Math.random() * (max - min + 1) - min)
-
+    n = Math.floor(Math.random() * (max - min + 1) + min)
+    console.log(n)
     return n;
+}
+
+function callback(event) {
+    let number = document.querySelector("[name=guess]")
+    let colpo = number.value
+
+    while(colpo === null || isNaN(colpo)) {
+        colpo = prompt("Idovina il numero!!!")
+    }
+    tentativi++
+    colpo = parseInt(colpo)
+
+    if(colpo == n) {
+        console.log("CONGRATURAZIONI! Hai vinto.")
+        tentativi = 0
+        alert("CONGRATURAZIONI! Hai vinto.")
+    } else if(colpo < n) {
+        console.log("Troppo piccolo. Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+    } else {
+        console.log("Troppo grande.  Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
+    }
 }
 
 getRandom(0, 100)
 
-function handleClick(event) { 
-
-    while(tentativi < 5) {
-        let colpo = document.querySelector("#guess")
-        console.log(userField.value)
-        while(colpo === null || isNaN(colpo)) {
-            colpo = prompt("Idovina il numero!!!")
-        }
-        tentativi++
-        colpo = parseInt(colpo)
-
-        if(colpo == n) {
-            console.log("CONGRATURAZIONI! Hai vinto.")
-            tentativi = 5
-        } else if(colpo < n) {
-            console.log("Troppo piccolo. Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
-        } else {
-            console.log("Troppo grande.  Tentativi rimasti: " + (5 - tentativi) + " | " + colpo)
-        }
-    }
-
-    alert("Hai perso. Il numero da indovinare era: " + n)
+let button = document.querySelector("[type=button]")
+while(tentativi != 0) { 
+    button.addEventListener("click", callback)
+    tentativi = tentativi - 1
 }
-
-btn.addEventListener("click", handleClick)
